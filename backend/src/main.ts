c4a7da7871750779be1216ce42368f167566c0f4
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-09-05 23:18:56 -0400

    feat(backend): add swagger module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Luna')
+    .setDescription('The Luna lua module host API description')
+    .setVersion('0.1')
+    .addTag('luna')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
