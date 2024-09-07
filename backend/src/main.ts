b8db7024e7d73052fe49dffb7319e6b413fdbe76
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-09-07 07:21:49 -0400

    feat(backend): add helmet for improved security through HTTP headers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.use(helmet());
   app.enableCors({
     origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
