bcfa16349af0397268fb1abde17001a2849d4a41
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-09-07 04:20:03 -0400

    feat(backend): add CORS security (wip)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,13 @@ import { setupSwagger } from './swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    maxAge: 86400,
+  });
 
   setupSwagger(app);
 
