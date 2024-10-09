277a0f989ccbb279016704d76ecdb569b0a6c55b
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-10-09 06:04:45 -0400

    ref!: use devenv instead of flakes, this aleviates the pain and workarounds around PostgreSQL

---
+++ devenv.nix
@@ -0,0 +1,47 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  dotenv.disableHint = true;
+
+  # Packages
+  packages = with pkgs; [
+    just # Command-runner
+  ];
+
+  # Languages
+  languages.javascript = {
+    enable = true;
+    bun.enable = true;
+  };
+  languages.typescript.enable = true;
+
+  # Services
+  services.postgres = {
+    enable = true;
+    initialDatabases = [
+      # XXX: add custom user and password for the database?
+      { name = "luanox_test"; }
+    ];
+    settings = {
+      log_connections = true;
+      log_statement = "all";
+      logging_collector = true;
+      log_disconnections = true;
+      log_destination = lib.mkForce "syslog";
+    };
+  };
+
+  enterShell = ''
+  echo "Start Postgres with 'devenv up -d' and stop it using 'devenv processes stop'."
+  '';
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  # '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
