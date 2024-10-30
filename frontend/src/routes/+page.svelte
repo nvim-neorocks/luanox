45237581ac11e31c8efe4a54ca8326b800aa19ae
vhyrro <vhyrro@gmail.com>
2024-10-30 19:12:32 +0100

    chore: more boilerplate

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+    import NavBar from "$lib/NavBar.svelte";
+</script>
+
+<NavBar />
+
 <h1>Welcome to Luanox</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa laudantium voluptatibus rem tempore ut laboriosam id esse eveniet voluptatem voluptatum reiciendis nemo, provident nihil accusamus, similique nam perferendis cupiditate accusantium commodi corporis ex recusandae nisi sit? Quam facere eius voluptas et molestiae magni quibusdam incidunt quod provident enim saepe accusantium, voluptate, corporis dicta nemo? Cumque nobis cum dolor inventore libero ipsam qui, dolorem ea repudiandae. Vero minus, doloremque itaque, error saepe tenetur praesentium amet in iusto animi sint optio laudantium, a odio similique neque inventore impedit nemo rerum. Ratione, incidunt omnis adipisci illum repudiandae nemo temporibus ut modi sint alias!</p>
+
+
