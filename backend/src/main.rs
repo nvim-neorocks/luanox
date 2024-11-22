56366653d10224e5e636919b1309874f3fd8e6c2
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-11-22 14:24:37 -0400

    ref(backend)!: convert postgres connection into a connection pool
    These changes also exposes the database to the Actix server as an app data so the routes can actually access and interact with it.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,8 @@
-use actix_web::{App, HttpServer};
-use diesel::Connection as _;
+use actix_web::{web, App, HttpServer};
 use std::io;
 
 mod v1;
+mod db;
 mod schema;
 
 #[actix_web::main]
@@ -14,15 +14,20 @@ pub async fn main() -> io::Result<()> {
     let db_url =
         std::env::var("DATABASE_URL").expect("no DATABASE_URL specified. Is your `.env` loaded?");
 
-    let _db = diesel::PgConnection::establish(&db_url).expect("unable to connect to database");
+    let db = db::initialize_db_pool(db_url);
+
+    // XXX: just for dev purposes, my brain cannot handle a successful restart w/o messages to stdout
+    println!("Starting server on port {}", port);
 
     // TODO: Hook up the backend properly, currently we just bind `index()` to `/api/`
-    HttpServer::new(|| App::new().service(actix_web::web::scope("/api").service(v1::index)))
-        .bind((url, port.parse().unwrap()))
-        .unwrap()
-        .run()
-        .await
-        .unwrap();
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(db.clone()))
+            .service(actix_web::web::scope("/api").service(v1::index))
+    })
+    .bind((url, port.parse().unwrap()))?
+    .run()
+    .await?;
 
     Ok(())
 }
