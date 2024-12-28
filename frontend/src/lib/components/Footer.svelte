5c7ea797ee4ab534b0898da9c5bc0270b137763a
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-12-28 14:44:15 -0400

    feat(frontend): hello landing page :D
    This commit has A LOT of changes I've completely forgotten to bisect, but some of the most relevant changes are:
    - Better structure for components
    - Add Melt UI preprocessor
    - Add Luanox SVG logo and improve the favicon logic for better cross-platform compatibility
    - Finish the NavBar component
    - Make the landing page functional based on the penpot design

---
+++ frontend/src/lib/components/Footer.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { IconBrandGithub, IconServer2 } from "@tabler/icons-svelte";
+</script>
+
+<footer class="bg-[#303040] py-4 px-6 w-full">
+  <div
+    class="container mx-auto flex justify-between items-center text-xs text-[#86919E]"
+  >
+    <div class="flex items-center">
+      <span
+        >Copyright &copy; {new Date().getFullYear()}
+        <a
+          href="https://github.com/nvim-neorocks/luanox"
+          class="text-blue-400 hover:underline">nvim-neorocks</a
+        >, Licensed under AGPL</span
+      >
+    </div>
+    <div class="flex items-center">
+      <a
+        href="https://github.com/nvim-neorocks/luanox"
+        class="flex items-center mr-4 hover:text-white"
+      >
+        <IconBrandGithub size={20} class="mr-1" />
+        <span>GitHub</span>
+      </a>
+      <a href="/status" class="flex items-center mr-4 hover:text-white">
+        <IconServer2 size={20} class="mr-1" />
+        <span>Status</span>
+      </a>
+    </div>
+  </div>
+</footer>
