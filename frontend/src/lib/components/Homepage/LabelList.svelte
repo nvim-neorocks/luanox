cb715080c66be56dda332df8c94c94a8a29e5632
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-12-29 14:58:28 -0400

    feat(frontend): add sweetie colorscheme, theme toggle and some other QoL improvements I've forgotten

--- frontend/src/lib/components/Homepage/LabelList.svelte
+++ frontend/src/lib/components/Homepage/LabelList.svelte
@@ -12,10 +12,10 @@
   <h2 class="text-xl font-semibold mb-4">{title}</h2>
   <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
     {#each labels as label}
-      <div class="bg-[#25252f] rounded-md p-3">
+      <div class="bg-surface rounded-md p-3">
         <div class="flex justify-between items-center">
           <p>{label.name}</p>
-          <span class="text-gray-400 text-sm">{label.count} modules</span>
+          <span class="text-grey text-sm">{label.count} modules</span>
         </div>
       </div>
     {/each}
