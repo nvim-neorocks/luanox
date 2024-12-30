2320d2ac36af3129f3dab8e1a874fab124be8d09
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-12-30 08:54:10 -0400

    ref(navbar): general navbar tweaks and changes
    Changes made:
    - Make the logo part of the link to the homepage
    - Remove underline from links
    - Tweak dropdowns styling

--- frontend/src/lib/components/NavBar.svelte
+++ frontend/src/lib/components/NavBar.svelte
@@ -46,9 +46,11 @@
 
 <nav class="bg-base-alt border-b border-base-alt text-text">
   <div class="container px-6 py-3 flex justify-between items-center">
-    <div class="flex items-center">
-      <LuanoxIcon class="h-6 w-auto mr-2" />
-      <a href="/" class="font-medium text-lg invisible md:visible">Luanox</a>
+    <div class="font-medium text-lg">
+      <a href="/" class="flex items-center"
+        ><LuanoxIcon class="h-6 w-auto mr-2" />
+        <span class="invisible md:visible">Luanox</span></a
+      >
     </div>
 
     <div class="flex items-center space-x-6">
@@ -61,13 +63,13 @@
       </button>
       <a
         href="/docs"
-        class="text-grey hover:text-blue hover:underline flex items-center"
+        class="flex items-center font-medium text-grey hover:text-blue"
       >
         <IconBook2 size={20} class="mr-1" /> Docs
       </a>
       <button
         type="button"
-        class="text-grey hover:text-blue hover:underline flex items-center"
+        class="flex items-center font-medium text-grey hover:text-blue"
         use:melt={$createTrigger}
         aria-label="Open create section dropdown"
       >
@@ -75,24 +77,26 @@
       </button>
       {#if $createOpen}
         <div
-          class="absolute right-0 top-14 w-48 bg-surface border border-dark-grey rounded-md shadow-lg py-1 z-10"
+          class="absolute right-0 top-14 w-48 bg-surface border border-dark-grey rounded-md shadow-lg z-10"
           use:melt={$createMenu}
         >
           {#each createContent as content}
             <a
               href={content.href}
-              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt
-            hover:text-text-alt"
+              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-blue"
               use:melt={$createItem}>{content.label}</a
             >
           {/each}
-          <div class="border-t border-l border-dark-grey" use:melt={$createArrow}></div>
+          <div
+            class="border-t border-l border-dark-grey"
+            use:melt={$createArrow}
+          ></div>
         </div>
       {/if}
 
       <button
         type="button"
-        class="text-grey hover:text-blue hover:underline flex items-center"
+        class="flex items-center font-medium text-grey hover:text-blue"
         use:melt={$accountTrigger}
         aria-label="Open account section dropdown"
       >
@@ -100,45 +104,48 @@
       </button>
       {#if $accountOpen}
         <div
-          class="absolute right-0 top-14 w-48 bg-surface border border-dark-grey rounded-md shadow-lg py-1 z-10"
+          class="absolute right-0 top-14 w-48 bg-surface border border-dark-grey rounded-md shadow-lg z-10"
           use:melt={$accountMenu}
         >
           {#if loggedIn}
             <a
               href="/user/profile"
-              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-text-alt"
+              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-blue"
               use:melt={$accountItem}>Profile</a
             >
             <a
               href="/user/settings"
-              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-text-alt"
+              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-blue"
               use:melt={$accountItem}>Settings</a
             >
             <a
               href="/user/reports"
-              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-text-alt"
+              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-blue"
               use:melt={$accountItem}>Reports</a
             >
             <a
               href="/logout"
-              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-text-alt"
+              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-blue"
               use:melt={$accountItem}>Logout</a
             >
           {:else}
             <div
-              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-text-alt"
+              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-blue"
               use:melt={$accountItem}
             >
               Login
             </div>
             <div
-              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-text-alt"
+              class="block px-4 py-2 text-sm text-grey hover:bg-base-alt hover:text-blue"
               use:melt={$accountItem}
             >
               Register
             </div>
           {/if}
-          <div class="border-t border-l border-dark-grey" use:melt={$accountArrow}></div>
+          <div
+            class="border-t border-l border-dark-grey"
+            use:melt={$accountArrow}
+          ></div>
         </div>
       {/if}
     </div>
