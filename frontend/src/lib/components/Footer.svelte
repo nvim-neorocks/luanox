3acd768b6bb00e5d441ff69004f14d4816fbf1ea
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-12-30 09:04:59 -0400

    fix(footer): make text bigger on desktop view

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -4,7 +4,7 @@
 
 <footer class="bg-base-alt py-4 px-6 w-full">
   <div
-    class="container flex justify-between items-center font-medium text-xs text-grey"
+    class="container flex justify-between items-center font-medium text-xs md:text-sm text-grey"
   >
     <div class="flex items-center">
       <span
