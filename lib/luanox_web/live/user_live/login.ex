0659ab0f8fa8f6f778aa64bb4f075588881d043e
NTBBloodbath <bloodbathalchemist@protonmail.com>
2025-05-19 11:46:58 -0400

    feat(login): center the OAuth view, add github icon

--- lib/luanox_web/live/user_live/login.ex
+++ lib/luanox_web/live/user_live/login.ex
@@ -4,27 +4,30 @@ defmodule LuaNoxWeb.UserLive.Login do
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash} current_scope={@current_scope}>
-      <div class="mx-auto max-w-sm space-y-4 py-8">
-        <.header class="text-center">
-          <p>Choose an OAuth Provider</p>
-          <:subtitle>
-            <%= if @current_scope do %>
-              You need to reauthenticate to perform sensitive actions on your account.
-            <% else %>
-              We don't support password logins for security reasons.
-            <% end %>
-          </:subtitle>
-        </.header>
+      <%!-- The height calculation inherits both the navbar and footer sizes in px --%>
+      <div class="flex h-[calc(100vh-116px)] md:h-[calc(100vh-124px)]">
+        <div class="max-w-sm space-y-4 py-8 m-auto">
+          <.header class="text-center">
+            <p>Choose an OAuth Provider</p>
+            <:subtitle>
+              <%= if @current_scope do %>
+                You need to reauthenticate to perform sensitive actions on your account.
+              <% else %>
+                We don't support password logins for security reasons.
+              <% end %>
+            </:subtitle>
+          </.header>
 
-        <div class="flex flex-col space-y-4">
-          <.link
-            class="btn btn-primary"
-            id="github-login"
-            phx-disable-with="Redirecting..."
-            navigate={~p"/auth/github"}
-          >
-            <%!-- <.icon name={:github} type={:filled} class="mr-2" /> --%> GitHub
-          </.link>
+          <div class="flex flex-col space-y-4">
+            <.link
+              class="btn btn-primary"
+              id="github-login"
+              phx-disable-with="Redirecting..."
+              navigate={~p"/auth/github"}
+            >
+              <.icon name={:brand_github} type={:filled} class="size-5" /> GitHub
+            </.link>
+          </div>
         </div>
       </div>
     </Layouts.app>
