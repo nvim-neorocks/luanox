ecc574b1c3c8372c722752d042200b79e0600918
vhyrro <vhyrro@gmail.com>
2025-06-02 14:49:30 +0200

    feat: revamp sizing on frontend

--- luanox/lib/luanox_web/components/package_box.ex
+++ luanox/lib/luanox_web/components/package_box.ex
@@ -9,11 +9,11 @@ defmodule LuaNoxWeb.PackageBox do
   def package(assigns) do
     ~H"""
     <.link navigate={~p"/package/#{@name}"}>
-      <div role="button" class="card w-72 md:w-96 transition-colors ease-in duration-200 motion-reduce:transition-none motion-reduce:hover:transform-none bg-base-200 hover:bg-base-300 border border-dark-grey">
+      <div role="button" class="card-sm rounded-sm w-72 md:w-96 transition-colors ease-in duration-200 motion-reduce:transition-none motion-reduce:hover:transform-none bg-base-200 hover:bg-base-300 border border-dark-grey">
         <div class="card-body">
           <div class="flex justify-between">
             <h2 class="card-title">{@name}</h2>
-            <span class="font-semibold md:text-lg">v{@version}</span>
+            <span class="font-semibold">v{@version}</span>
           </div>
           <p class="justify-self-start">{@description}</p>
         </div>
@@ -26,9 +26,9 @@ defmodule LuaNoxWeb.PackageBox do
 
   def packages(assigns) do
     ~H"""
-    <div class="flex flex-col items-center py-4">
+    <div class="flex flex-col items-center py-8">
       <%!-- FIXME: this should show the most downloaded packages once we implement downloads count --%>
-      <h2 class="text-2xl font-bold mb-4">Featured Packages</h2>
+      <h2 class="text-2xl font-bold mb-8">Featured Packages</h2>
       <div class="flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap gap-4 items-center justify-center max-w-screen-lg md:w-full mb-12">
         <%= for %{name: name, summary: summary, releases: releases} <- assigns[:packages] |> Enum.take(4) do %>
           <.package
@@ -39,7 +39,7 @@ defmodule LuaNoxWeb.PackageBox do
           />
         <% end %>
       </div>
-      <h2 class="text-2xl font-bold mb-4">Most Recent Updates</h2>
+      <h2 class="text-2xl font-bold mb-8">Most Recent Updates</h2>
       <div class="flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap gap-4 items-center justify-center max-w-screen-lg md:w-full mb-12">
         <%= for %{name: name, summary: summary, releases: releases} <- assigns[:packages] |> Enum.take(4) do %>
           <.package
