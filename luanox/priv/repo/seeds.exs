33a30f10bb6e721b3631d0f3c48e477a0024f51a
vhyrro <vhyrro@gmail.com>
2025-08-29 18:10:36 +0200

    feat!: revamp landing page

--- luanox/priv/repo/seeds.exs
+++ luanox/priv/repo/seeds.exs
@@ -42,7 +42,7 @@ user =
 package =
   Repo.insert!(%Package{
     name: "luanox",
-    summary: "An epic Lua site",
+    summary: "A particularly long summary for the package to test line clamping in the UI. This summary is intentionally made long so that we can observe how the line clamping behaves in the user interface.",
     description: "Full description of the package",
     user_id: user.id
   })
