10d25aee15a98ac2e7fa9c102c690c4402389e81
vhyrro <vhyrro@gmail.com>
2025-08-31 20:08:00 +0200

    refactor: make the search bar in main page work and change footer message

--- luanox/lib/luanox/constants.ex
+++ luanox/lib/luanox/constants.ex
@@ -7,6 +7,8 @@ defmodule LuaNox.Constants do
   The Luanox organization name.
   """
   @luanox_org "The Neorocks Team"
+  @license "AGPL-3.0"
 
   def luanox_org, do: @luanox_org
+  def license, do: @license
 end
